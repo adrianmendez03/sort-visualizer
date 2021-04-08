76be24000087e5ac1a0fc926af7109b61a41af41
Adrian Mendez <adrianmndz328@gmail.com>
2021-04-08 16:10:46 -0500

    Selection Sort

---
+++ algos/selection.js
@@ -0,0 +1,20 @@
+// Time complexity 0(n^2) as there are two nested loops
+// Space Complexity 0(1)
+
+
+function selectionSort (arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let minIdx = i
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[minIdx] > arr[j]) {
+                minIdx = j
+            }
+        }
+        [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]]
+    }
+    return arr
+}
+
+const arr = [4, 1, 5, 2, 7, 4, 10]
+
+console.log(selectionSort(arr))
