9c1031347f7cd88a6461e1b871623b9b8f33fc65
Adrian Mendez <adrianmndz328@gmail.com>
2021-04-09 16:44:39 -0500

    Responsive Rectabngle things

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 (function() {
-    let canvas, ctx, bottom, nums = generateNums()
+    let canvas, ctx, step, max = 50, nums = generateNums()
 
     function generateNums () {
         const unique = new Set()
-        while (unique.size < 10) {
-            unique.add(Math.floor(Math.random() * 10) + 1)
+        while (unique.size < max) {
+            unique.add(Math.floor(Math.random() * max) + 1)
         }
         return [...unique]
     }
 
     function createGrid () {
-        const step = 50
 
         const width = canvas.width
         const height = canvas.height
+
+        step = height / max
 
         ctx.save()
         ctx.strokeStyle = 'gray'
@@ -21,26 +22,26 @@
         ctx.font = '14px Monospace'
         ctx.lineWidth = 0.35
 
-        for (let y = 0; y < height; y += step) {
+        for (let y = 0; y <= max; y++) {
             ctx.beginPath()
-            ctx.moveTo(0, y)
-            ctx.lineTo(width, y)
+            ctx.moveTo(0, y * step)
+            ctx.lineTo(width, y * step)
             ctx.stroke()
-
-            ctx.fillText(y, 0, y)
+            ctx.fillText(y, 0, y * step)
         }
     }
 
     function drawNums () {
+        let width = Math.floor((canvas.width - 25) / max)
+        const gap = width * 0.01
         for (let i = 0; i < nums.length; i++) {
-            ctx.fillRect(25 + (55 * i),  bottom - 30, 50, -50 * nums[i])
+            ctx.fillRect(25 + (width) * i,  0, width - gap, step * nums[i])
         }
     }
 
     function resizeCanvas () {
-        bottom = window.screen.height - 200
-        canvas.width = window.screen.width
-        canvas.height = bottom
+        canvas.width = window.innerWidth
+        canvas.height = 500
         createGrid()
     }
 
