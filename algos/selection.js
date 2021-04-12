92e2d4463badac3e47425c5df593bb2a43b929a9
Adrian Mendez <adrianmndz328@gmail.com>
2021-04-12 15:22:06 -0500

    Added Descript to Selection Sort

--- algos/selection.js
+++ algos/selection.js
@@ -1,6 +1,9 @@
 // Time complexity 0(n^2) as there are two nested loops
 // Space Complexity 0(1)
 
+// Selection Sort loops through the entire array to find the smallest value and places it at the beginning. It does this until it reaches the end of the array.
+// On avereage this sorting method time complexity is n^2, but what it lacks in speed it makes up for in Space as it never makes for than 0(n) swaps making
+// it useful when working with limited memory. 
 
 function selectionSort (arr) {
     for (let i = 0; i < arr.length; i++) {
