041f9452a99190d918498d4eb84f3ccdf283d026
Adrian Mendez <adrianmndz328@gmail.com>
2021-04-12 15:10:35 -0500

    Added Description for Insertion Sort

--- algos/insertion.js
+++ algos/insertion.js
@@ -1,6 +1,11 @@
 // Time Complexity 0(n^2)
 // Space Complexity 0(1)
 
+// Insertion Sort works similiarly to the way you would sort playing cards in your hands.
+// Given a list of numbers it starts at arr[1] and compares the current number to the previous number.
+// If the current number is less than, it swaps places until the current number is greater than the previous.
+// This process continues until it has iterated through the entire array.
+
 function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
         const key = arr[i]
